497387ca5bc0a055f54b4f5eeacc0c52c3db49f7
rahulclilmbai <rahul@climbai.co>
2025-02-11 16:35:06 +0530

    Remove Firebase

--- src/db/database.py
+++ src/db/database.py
@@ -1,3 +1,3 @@
 from google.cloud import firestore
 
-db = firestore.Client.from_service_account_json('./climb-ai-firebase-adminsdk-fbsvc-f3d3bbdabd.json')
+db = firestore.Client.from_service_account_json('./climb-ai-firebase.json')
